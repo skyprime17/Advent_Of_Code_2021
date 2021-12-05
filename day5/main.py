1d52587790c8ee86b59e4ff0b8147dbd65863259
skyprime17 <nmoeller@live.de>
2021-12-05 11:53:18 +0100

    - updated day 5, added option for 5.1

--- day5/main.py
+++ day5/main.py
@@ -1,11 +1,15 @@
 import re
 
 with open('input.txt') as f:
-    res = [i.rstrip() for i in f.readlines()]
+    res = [i for i in f.readlines()]
 
-def main():
-    res2 = [re.findall('\d+', i) for i in res]
+
+def crosses(res2, diagonals=True):
     index = {}
+
+    if not diagonals:
+        res2 = [c for c in res2 if c[0] == c[2] or c[1] == c[3]]
+        
     for c in res2:
         x1 = int(c[0])
         x2 = int(c[2])
@@ -16,7 +20,7 @@ def main():
         dy = 1 if y2 > y1 else 0 if y2 == y1 else -1
 
         while True:
-            if(x1==x2+dx) and (y1==y2+dy):
+            if(x1 == x2+dx) and (y1 == y2+dy):
                 break
             if(x1, y1) in index:
                 index[(x1, y1)] += 1
@@ -24,8 +28,14 @@ def main():
                 index[(x1, y1)] = 0
             x1 += dx
             y1 += dy
-    s = sum(value > 0 for _, value in index.items())
-    print(s)
+
+    return sum(value > 0 for _, value in index.items())
+
+def main():
+    res2 = [re.findall('\d+', i) for i in res]
+
+    print(crosses(res2, diagonals=False))
+    print(crosses(res2))
 
 
 if __name__ == '__main__':
